70f9bea09523f5d33089603e751a0d10ccd3152b
IA-Cross <ayanegui.isaac@gmail.com>
2021-04-09 10:57:37 -0500

    Configuracion Base de datos SICTED en AWS

--- src/main/java/com/integrador/sicdet/repository/TuserRepository.java
+++ src/main/java/com/integrador/sicdet/repository/TuserRepository.java
@@ -7,15 +7,5 @@ import java.util.List;
 
 @Repository
 public interface TuserRepository extends JpaRepository<Tuser,Integer>{
-    List<Tuser> findUserPageOrder();
 
-    Tuser findByEmail(String email);
-
-    Tuser findByOtherIdentifier(String identifier);
-
-    boolean existsByEmail(String email);
-
-    boolean existsByOtherIdentifier(String identifier);
-
-    Tuser findByOtherIdentifierAndTokenLastTen(String identifier, String hashCode);
 }
